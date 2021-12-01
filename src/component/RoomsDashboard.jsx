a0ebb9dd0fd95f46188056542c40e290ebd94272
Ashwin Aashu <80351416+AshwinAashu@users.noreply.github.com>
2021-12-01 19:17:25 +0530

    RoomsDashboard --setup

--- src/component/RoomsDashboard.jsx
+++ src/component/RoomsDashboard.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 const RoomsDashboard = () => {
+  const [rooms, setRooms] = useState({});
+
   return (
     <div className="dashboard-main">
       <div className="dashboard-container">
         <div className="dashboard-head">
-          <span className="title-main">Rooms</span>
+          <span className="title-main rooms-heading">Rooms</span>
           <button className="btn-primary add-rooms">Add a room</button>
         </div>
+        <hr className="head-divider" />
       </div>
     </div>
   );
