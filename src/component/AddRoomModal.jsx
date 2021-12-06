68deedbfc6c506f9969424df9e7169ea72ce22f7
Ashwin Aashu <80351416+AshwinAashu@users.noreply.github.com>
2021-12-06 15:25:18 +0530

    Addmodal --css

--- src/component/AddRoomModal.jsx
+++ src/component/AddRoomModal.jsx
@@ -1,20 +1,33 @@
+import { useNavigate } from "react-router-dom";
 const AddRoomModal = ({ setRoomName, showModal }) => {
+  let name;
   //Show modal False
+  let navigate = useNavigate();
   if (!showModal) {
     return null;
   }
   //set roomName
   const handleRoomName = (e) => {
-    let name = e.target.value();
-    setRoomName(name);
+    name = e.target.value();
   };
   return (
     <div className="new-room-modal">
       <div className="modal-main">
         <div className="modal-head">Create a new room</div>
-        <div className="modal-container">drop image here</div>
+        <div className="modal-container">
+          <span>drop image here</span>
+        </div>
         <div className="modal-footer">
           <input placeholder="Name your room" value={handleRoomName} />
+          <button
+            className="btn-primary"
+            onSubmit={() => {
+              setRoomName(name);
+              navigate("/dashboard");
+            }}
+          >
+            Save Room
+          </button>
         </div>
       </div>
     </div>
--- src/component/CreateRooms.jsx
+++ src/component/CreateRooms.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import AddRoomModal from "./AddRoomModal";
 const CreateRooms = () => {
+  //Room Modal
+  const [showModal, setShowModal] = useState();
+  const handleModal = () => {
+    setShowModal(true);
+  };
+
+  //navigation
   let navigate = useNavigate();
   const handleRedirect = () => {
     navigate("/dashboard");
@@ -14,8 +23,9 @@ const CreateRooms = () => {
             Create a room or explore your saved rooms
           </span>
           <br />
-          <button className="btn-primary create-rooms">
+          <button className="btn-primary create-rooms" onClick={handleModal}>
             Create a new room
+            <AddRoomModal showModal={showModal} />
           </button>
           <br />
           <button className="btn-primary create-rooms" onClick={handleRedirect}>
